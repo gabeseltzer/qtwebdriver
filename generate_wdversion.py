85736985d2a89d3ed0c65444eb83b31c669ce59e
Gabriel Seltzer <gabriel.seltzer@formlabs.com>
2025-11-03 19:18:59 +0000

    use python3 decoding

--- generate_wdversion.py
+++ generate_wdversion.py
@@ -5,13 +5,13 @@ p = sp.Popen(["git", "describe" , "--abbrev=10", "--dirty", "--always"], stdin=s
 
 data = p.stdout.readline()
 
-versionfile = open (filename, 'w')
+versionfile = open (filename, "w")
 versionfile.write("namespace webdriver {\n")
 versionfile.write("extern const char kProductName[] = \"WebDriver-cisco-cmt\";\n")
 versionfile.write("extern const char kVersionNumber[] = \"1.3.3\";\n")
 versionfile.write("extern const char kBuildTime[] = __TIME__;\n")
 versionfile.write("extern const char kBuildDate[] = __DATE__;\n")
-versionfile.write("extern const char kLastChanges[] = \"" + data.strip() + "\";\n")
+versionfile.write("extern const char kLastChanges[] = \"" + data.strip().decode("utf-8") + "\";\n")
 versionfile.write("}")
 versionfile.close()
 
